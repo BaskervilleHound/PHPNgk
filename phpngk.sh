2e1b7ff357372aac3d94597fca1960c71ee1552c
BaskervilleHound <93882284+BaskervilleHound@users.noreply.github.com>
2022-11-01 10:18:58 +0100

    Fixing errors and formatting

--- phpngk.sh
+++ phpngk.sh
@@ -1,75 +1,110 @@
 #!/bin/bash
 
-openphpfile ()
-{
-  if[$browser == "firefox"]
-    then
+openphpfile () {
+  if [ $browser == "firefox" ]
+  then
 ## Firefox
-      if[[$2 == "-n" || $2 == "-nq"]]
-      then url = $browser " " $nlink
-      else
-      url = $browser " localhost:$localport/$file" fi
-      else
+    if [[ $2 == "-n" || $2 == "-nq" ]]
+    then
+      	url="$browser $nlink/$file"
+	echo "URL:" $url
+	echo "File:" $file
+    else
+      	url=$browser" localhost:$localport/$file"
+    fi
+  else
 ## Chrome
-    if[[$2 == "-n" || $2 == "-nq"]]
-      then url = "$browser -disable-gpu http://$nlink"
-      else
-    url = "$browser -disable-gpu http://localhost:$localport/$file" fi fi $localphp - S localhost:$localport | eval
-	"$url"}
+    if [[ $2 == "-n" || $2 == "-nq" ]]
+    then
+      	url="$browser -disable-gpu http://$nlink/$file"
+    else
+    	url="$browser -disable-gpu http://localhost:$localport/$file"
+    fi
+  fi
 
-      openngrok (){
-  ngrok http 8080-- log stdout > ngrok.log & sleep 1 nlink = $ (curl-- silent-- show - error http:	//127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
-echo "LINK: $nlink" echo "$nlink" | xclip - selection clipboard
-rm ngrok.log
+$localphp -S localhost:$localport | eval "$url"
+}
+
+openngrok (){
+  ngrok http 8080 --log stdout > ngrok.log & sleep 1
+  nlink=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
+  echo "LINK: $nlink"
+  echo "$nlink" | xclip -selection clipboard
+  rm ngrok.log
 }
 
 generateqr (){
-qrencode - o qrcode.png "$nlink" feh qrcode.png &
+  qrencode -o qrcode.png "$nlink"
+  feh qrcode.png &
 }
 
 checkfile (){
-if[!-f "$file"] then echo "ERROR: WRONG FILENAME" exit 1 fi
+  if [ ! -f "$file" ]
+  then
+  echo "ERROR: WRONG FILENAME"
+  exit 1
+  fi
 }
 
 showhelp (){
 echo " "
 echo "Usage:"
-echo
-"Standard: pass a file as first arg to open it with local PHP on browser and port (default Firefox at localhost:8080)"
-echo
-"-n: turns on ngrok. The link generated is opened on browser, shown on console and copied to your clipboard"
-echo
-"-nq: turns on ngrok, show the link, copies it to your clipboard and generate a QR code in current dir. Automatically opened with feh."
-echo "-h | --help: shows this menu" echo
-"-c | --config: opens the script in nano" echo
+echo "Standard: pass a file as first arg to open it with local PHP on browser and port (default Firefox at localhost:8080)"
+echo "-n: turns on ngrok. The link generated is opened on browser, shown on console and copied to your clipboard"
+echo "-nq: turns on ngrok, show the link, copies it to your clipboard and generate a QR code in current dir. Automatically opened with feh."
+echo "-h | --help: shows this menu"
+echo "-c | --config: opens the script in nano"
 echo " "
-"Example: phpngk index.php -n" echo
-"(All options can be configured on the script to achieve the desired behaviour)"
+echo "Example: phpngk index.php -n"
+echo "(All options can be configured on the script to achieve the desired behaviour)"
 echo " "
 echo "Current PHP local route = $localphp"
 echo "Current port to be opened = $localport"
 }
 
-file = $1
+file=$1
 #Installed in local:
-localphp = "php"
-#localphp="/opt/lampp/bin/php"
-localport = "8080"
-browser = "firefox"
+#localphp="php"
+localphp="/opt/lampp/bin/php"
+localport="8080"
+browser="firefox"
 #browser="google-chrome"
-if[["$file" == "-h" || "$file" == "--help" || -z "$1"]]
+if [[ "$file" == "-h" || "$file" == "--help" || -z "$1" ]]
 then
 showhelp
-exit 0 fi if[["$file" == "-c" || "$file" == "--config"]] then nano $ { 0 }
-exit 1 fi case $2 in - n)
+exit 0
+fi
+
+if [[ "$file" == "-c" || "$file" == "--config" ]]
+then
+nano "${0}"
+exit 1
+fi
+
+case $2 in 
+-n)
 #Opening ngrok at the port where PHP is running.
 #Then we curl the url generated and generate a QR image with it, which it's opened.
-	checkfile openngrok openphpfile "$@";;
-      -nq) checkfile openngrok;
-      generateqr openphpfile "$@";;
-      -h | --help) showhelp;;
-      *)if[-z "$2"]
-	then checkfile openphpfile "$@"
-	else
-	  showhelp fi;;
-  esac
+checkfile
+openngrok
+openphpfile "$@"
+;;
+-nq)
+checkfile
+openngrok;
+generateqr
+openphpfile "$@"
+;;
+-h|--help)
+showhelp
+;;
+*)
+if [ -z "$2" ]
+then
+checkfile
+openphpfile "$@"
+else
+showhelp 
+fi
+;;
+esac
